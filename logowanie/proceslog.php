64707fb91827c0344fa959fac3c34f8a5506f078
stego20 <95852473+stego20@users.noreply.github.com>
2021-12-09 21:01:07 +0100

    Create proceslog.php

---
+++ logowanie/proceslog.php
@@ -0,0 +1,29 @@
+<?php
+include_once '..\db\conect.php';
+session_start();
+if(isset($_POST['submit'])){
+    
+    $login=$_POST['login'];
+    $haslo=$_POST['haslo'];
+    unset($_POST);
+    if ($haslo!='' and $login!=''){
+        $sql="SELECT * FROM konta WHERE `login`='$login' and `haslo`='$haslo'";
+        $rezultat=$polaczenie->query($sql);
+        $total=$rezultat->num_rows;
+        if($total==1){
+            $wiersz=$rezultat->fetch_assoc();
+            $_SESSION['user']=$wiersz['imie']." ".$wiersz['nazwisko'];
+            header('Location: ../');
+        }
+        else{
+            $_SESSION['blad']='Zły Login albo Hasło';
+            header("location: logowanie.php"); 
+        }
+    }
+    else{
+    $_SESSION['blad']='Zły Login albo Hasło';
+    header("location: logowanie.php"); 
+    }
+    
+}
+?>
