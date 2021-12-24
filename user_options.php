0fa320b7ffff595995d852fcd6f418cc002f91c4
Adrianek2003 <adrian.kunecki2003@gmail.com>
2021-12-24 15:43:23 +0100

    update

--- user_options.php
+++ user_options.php
@@ -37,6 +37,20 @@ $quiz2 = array(
     "score" => 84,
 );
 
+$quiz3 = array(
+    "name" => "Polski",
+    "maxpoint" => 28,
+    "score" => 12,
+);
+
+$quiz4 = array(
+    "name" => "Niemiecki",
+    "maxpoint" => 60,
+    "score" => 57,
+);
+
+$tests = array();
+array_push($tests, $quiz1, $quiz2, $quiz3, $quiz4);
 
 ?>
 
@@ -55,16 +69,20 @@ $quiz2 = array(
     <div class="tab-content" id="pills-tabContent">
         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
             <ul class="list-group">
-                <li class="list-group-item"><?php echo($quiz1["name"]) ?></li>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width: <?php echo($quiz1["score"]/$quiz1["maxpoint"]*100) ?>%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><?php echo($quiz1["score"]/$quiz1["maxpoint"]*100) ?>%</div>
-                </div>
-                <li class="list-group-item"><?php echo($quiz2["name"]) ?></li>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width: <?php echo($quiz2["score"]/$quiz2["maxpoint"]*100)?>%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><?php echo($quiz2["score"]/$quiz2["maxpoint"]*100) ?>%</div>
-                </div>
+                    <?php
+                    for ($i=0; $i < sizeof($tests); $i++) 
+                    { 
+                        $procent = number_format($tests[$i]["score"]/$tests[$i]["maxpoint"]*100, 2);
+                        echo("
+                        <li class='list-group-item'>{$tests[$i]["name"]}</li>
+                        <div class='progress'>
+                            <div class='progress-bar' role='progressbar' style='width: {$procent}%;' aria-valuenow='25' aria-valuemin='0' aria-valuemax='100'>{$procent}%</div>
+                        </div>
+                        ");
+                    }
+                    ?>
             </ul>            
-        </div>
+        </div>        
         <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
             <form action="user_options.php" method="POST">
                 <div class="accordion-item">
